a1ab7a6ea77bb0970501b213de97558d7929ec43
Bryan Yep <yepsan95@gmail.com>
2023-05-09 09:13:59 -0700

    fix: push() and pop() functions

--- main.c
+++ main.c
@@ -2,27 +2,26 @@
 
 /**
  * main - entry point
- * @ac: number of arguments
- * @av: array of arguments
  *
- * Return: 0 on success, -1 on error
+ * Return: 0 on success
  */
-int main(int ac, __attribute__((unused)) char **av)
+int main(void)
 {
 	stack_t *top = NULL;
-	int n;
-
-	if (ac < 2)
-		return (-1);
 
 	push(&top, 1);
 	push(&top, 2);
 	push(&top, 3);
 	push(&top, 4);
 	push(&top, 5);
+	push(&top, 6);
 
-	for (n = pop(&top); n; n = pop(&top))
-		printf("n = %d\n", n);
+	pop(&top);
+	pop(&top);
+	pop(&top);
+	pop(&top);
+	pop(&top);
+	pop(&top);
 
 	return (0);
 }
@@ -41,14 +40,14 @@ int push(stack_t **top, int value)
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 		return (-1);
-
 	new_node->n = value;
 	new_node->next = NULL;
 	new_node->prev = *top;
-	(*top)->next = new_node;
+	if (*top != NULL)
+		(*top)->next = new_node;
 	*top = new_node;
 
-	printf("%d\n", (*top)->n);
+	printf("push -> %d\n", (*top)->n);
 
 	return (1);
 }
@@ -70,12 +69,15 @@ int pop(stack_t **top)
 	value = (*top)->n;
 	tmp = *top;
 
-	(*top)->prev->next = NULL;
-	*top = (*top)->prev;
+	if ((*top)->prev != NULL)
+	{
+		(*top)->prev->next = NULL;
+		*top = (*top)->prev;
+	}
 
 	free(tmp);
 
-	printf("%d\n", value);
+	printf("pop -> %d\n", value);
 
 	return (value);
 }
