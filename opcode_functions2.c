e21e67dd6fee987fc37dce059ef35728572f7814
Bryan Yep <yepsan95@gmail.com>
2023-05-14 10:49:23 -0700

    feat: added nop opcode

---
+++ opcode_functions2.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - doesn't do anything
+ * @top: pointer to the last node of the stack
+ * @line_number: line_number in the monty file
+ *
+ * Return: void
+ */
+void nop(UNUSED stack_t **top, UNUSED unsigned int line_number)
+{
+	return;
+}
