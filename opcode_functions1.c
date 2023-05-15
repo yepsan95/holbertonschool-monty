75b50d8879c28844ca8a1369fd5829dedaf0a890
Bryan Yep <yepsan95@gmail.com>
2023-05-14 20:07:26 -0700

    fix: handled no argument case for push opcode

--- opcode_functions1.c
+++ opcode_functions1.c
@@ -31,7 +31,7 @@ void push(stack_t **top, unsigned int line_number)
 	}
 	if (word == NULL || only_numbers == 0)
 	{
-		dprintf(STDERR_FILENO, "L%d: usage: push integer\n", line_number);
+		dprintf(STDERR_FILENO, "L%d: usage: push integer\n", line_number + 1);
 		exit(EXIT_FAILURE);
 	}
 	value = atoi(word);
