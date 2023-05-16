4543982451f3d4cb15ee858d01e112f94656ed86
Bryan Yep <yepsan95@gmail.com>
2023-05-16 10:28:57 -0700

    fix: changed name of div() to div_()

--- opcode_functions2.c
+++ opcode_functions2.c
@@ -73,13 +73,13 @@ void sub(stack_t **top, unsigned int line_number)
  *
  * Return: void
  */
-void div(stack_t **top, unsigned int line_number)
+void div_(stack_t **top, unsigned int line_number)
 {
 	stack_t *tmp;
 
 	if (*top == NULL || top == NULL || (*top)->prev == NULL)
 	{
-		dprintf(STDERR_FILENO, "L%u: can't sub, stack too short\n", line_number + 1);
+		dprintf(STDERR_FILENO, "L%u: can't div, stack too short\n", line_number + 1);
 		exit(EXIT_FAILURE);
 	}
 
