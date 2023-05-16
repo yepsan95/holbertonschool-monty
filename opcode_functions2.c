7b5060a837346c56692e04fccc340c7427008288
Bryan Yep <yepsan95@gmail.com>
2023-05-16 10:05:23 -0700

    feat: added sub opcode

--- opcode_functions2.c
+++ opcode_functions2.c
@@ -37,3 +37,30 @@ void add(stack_t **top, unsigned int line_number)
 void nop(UNUSED stack_t **top, UNUSED unsigned int line_number)
 {
 }
+
+
+/**
+ * sub - substracts the top element of the stack
+ *       from the second top element of the stack
+ * @top: pointer to the last node of the stack
+ * @line_number: line_number in the monty file
+ *
+ * Return: void
+ */
+void sub(stack_t **top, unsigned int line_number)
+{
+	stack_t *tmp;
+
+	if (*top == NULL || top == NULL || (*top)->prev == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't add, stack too short\n", line_number + 1);
+		exit(EXIT_FAILURE);
+	}
+
+	tmp = *top;
+	*top = tmp->prev;
+	(*top)->next = NULL;
+	(*top)->n = tmp->n - (*top)->n;
+
+	free(tmp);
+}
